39cb562c8a4401ecb75fb7e591687fb48d54cd91
Stefan Dimitrov <stefan.r7.dimitrov@gmail.com>
2021-06-26 02:47:49 +0300

    IN  exercise

--- simple-queries/basic queries 05 - Between testing.sql
+++ simple-queries/basic queries 05 - Between testing.sql
@@ -3,9 +3,9 @@
 -- (include 30 and 50 in the results)
 
 SELECT * from "public"."customers"
-WHere age BETWEEN 30 and 50;
+WHERE age BETWEEN 30 and 50;
 
 -- What is the average income between the ages of 20 and 50? (Including 20 and 50)
 
-SELECt AVG(income) from "public"."customers"
-WHere age BETWEEN 20 and 50;
+SELECT AVG(income) from "public"."customers"
+WHERE age BETWEEN 20 and 50;
